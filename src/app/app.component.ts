3ca112c3f40f2e095d25c6ea8ad2551756953539
danieluche <dank.uche@yahoo.com>
2019-12-17 10:09:53 +0100

    bootstrap app routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-
 import * as firebase from 'firebase';
+
+const { FIREBASE_APIKEY, FIREBASE_AUTH_DOMAIN, FIREBASE_DATABASE_URL, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESAGING_SENDER_ID, FIREBASE_APP_ID } = process.env;
 
 
 const config = {
-  apiKey: 'YOUR_APIKEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  databaseURL: 'YOUR_DATABASE_URL',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
+  apiKey: FIREBASE_APIKEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    databaseURL: FIREBASE_DATABASE_URL,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID
 };
 
 @Component({
@@ -18,4 +21,7 @@ const config = {
 })
 export class AppComponent {
   title = 'blood-donor';
+  constructor() {
+    firebase.initializeApp(config);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddDonorComponent } from './components/add-donor/add-donor.component';
+import { DonorListComponent } from './components/donor-list/donor-list.component';
 
+const appRoutes: Routes = [
+  { path: 'donor-list', component: DonorListComponent },
+  { path: 'add-donor/:lat/:lng', component: AddDonorComponent },
+  { path: '',
+    redirectTo: '/donor-list',
+    pathMatch: 'full'
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddDonorComponent,
+    DonorListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
