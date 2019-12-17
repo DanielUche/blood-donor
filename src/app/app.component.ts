2b191444a803a266720b1e879204bdf9111f39bb
danieluche <dank.uche@yahoo.com>
2019-12-17 10:14:10 +0100

    add maps component to donor list page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import * as firebase from 'firebase';
 
-const { FIREBASE_APIKEY, FIREBASE_AUTH_DOMAIN, FIREBASE_DATABASE_URL, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESAGING_SENDER_ID, FIREBASE_APP_ID } = process.env;
+const { 
+  FIREBASE_APIKEY, 
+  FIREBASE_AUTH_DOMAIN, 
+  FIREBASE_DATABASE_URL, 
+  FIREBASE_PROJECT_ID, 
+  FIREBASE_STORAGE_BUCKET, 
+  FIREBASE_MESAGING_SENDER_ID, 
+  FIREBASE_APP_ID 
+} = process.env;
 
 
 const config = {
   apiKey: FIREBASE_APIKEY,
-    authDomain: FIREBASE_AUTH_DOMAIN,
-    databaseURL: FIREBASE_DATABASE_URL,
-    projectId: FIREBASE_PROJECT_ID,
-    storageBucket: FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: FIREBASE_MESAGING_SENDER_ID,
-    appId: FIREBASE_APP_ID
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  databaseURL: FIREBASE_DATABASE_URL,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID
 };
 
 @Component({
